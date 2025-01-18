e2a1b3cf144794058d8fb0c72ab5f7c7a8f40ce4
Hectorr <thehectorr.05@gmail.com>
2025-01-18 08:56:09 -0400

    base_calculadora

---
+++ calcu.py
@@ -0,0 +1,28 @@
+uno = 0.0
+dos = 0
+op = ""
+res = 0.0
+
+
+print("Hola, Bienvenido!")
+uno = float(input("Ingresa un número: "))
+dos = float(input("Ingresa otro número: "))
+op = input("Ingresa la operación (+, -, *, /): ")
+
+if op == "+":
+    res = uno + dos
+elif op == "-":
+    res = uno - dos
+elif op == "*":
+    res = uno * dos
+elif op == "/":
+    if dos != 0:
+        res = uno / dos
+    else:
+        print("Error: División por cero")
+else:
+    print("Operación no válida")
+
+print(f"El resultado de {uno} {op} {dos} es {res}")
+
+
